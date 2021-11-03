7f5d621e3ed580c4035902073d46e411657a904f
dpacker123 <89939888+dpacker123@users.noreply.github.com>
2021-11-03 14:13:51 -0400

    forced save

--- src/main/java/bsu/comp152/monwedcalc/CalcWindow.java
+++ src/main/java/bsu/comp152/monwedcalc/CalcWindow.java
@@ -11,10 +11,15 @@ public class CalcWindow {
 
     @FXML
     public void numberButtonPressed(ActionEvent event){
-        var pressedButton = (Button)event.getSource();
-        var buttonText = pressedButton.getText();
+        var pressedButton = (Button)event.getSource(); // this gets the number of the button that is displayyed on the btton
+        var buttonText = pressedButton.getText(); // this whole little bundle of code
         var currentNumberText = numberField.getText();
         numberField.setText(currentNumberText+buttonText);
 
+    }
+
+    @FXML
+    public void clearButtonPressed(){
+        numberField.clear();
     }
 }
