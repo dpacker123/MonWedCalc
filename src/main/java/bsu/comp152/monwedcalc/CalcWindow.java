de714cc2b921f5f771860850bc1d2684020884f0
dpacker123 <89939888+dpacker123@users.noreply.github.com>
2021-11-03 15:00:59 -0400

    yuh

--- src/main/java/bsu/comp152/monwedcalc/CalcWindow.java
+++ src/main/java/bsu/comp152/monwedcalc/CalcWindow.java
@@ -6,6 +6,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class CalcWindow {
+    private long firstNumber;
+    private OperatorType operation;
+
     @FXML
     private TextField numberField;
 
@@ -22,4 +25,54 @@ public class CalcWindow {
     public void clearButtonPressed(){
         numberField.clear();
     }
+
+    @FXML
+    public void equalPressed(){
+        var secondNumberText = numberField.getText();
+        var secondNumber = Long.parseLong(secondNumberText);
+        var result = 0.0;
+        switch(operation){
+            case Add -> result = secondNumber+firstNumber;
+            case Subtract -> result = firstNumber - secondNumber;
+            case Multiply -> result = firstNumber*secondNumber;
+            case Divide -> result = (double)firstNumber/secondNumber; // forces it to be a double
+        }
+        numberField.setText(""+result); // turns it back into a string
+    }
+
+    @FXML
+    public void plusPressed(){
+       var numberAsText =  numberField.getText(); // grabbed test from number field
+       firstNumber = Long.parseLong(numberAsText); // stored it as an int instead of string
+       numberField.setText("");
+       operation = OperatorType.Add;
+
+
+    }
+
+    @FXML
+    public void subtractPressed(){
+        var numberAsText =  numberField.getText(); // grabbed test from number field
+        firstNumber = Long.parseLong(numberAsText); // stored it as an int instead of string
+        numberField.setText("");
+        operation = OperatorType.Subtract;
+    }
+
+    @FXML
+    public void multiplyPressed(){
+        var numberAsText =  numberField.getText(); // grabbed test from number field
+        firstNumber = Long.parseLong(numberAsText); // stored it as an int instead of string
+        numberField.setText("");
+        operation = OperatorType.Multiply;
+    }
+
+    @FXML
+    public void dividePressed(){
+        var numberAsText =  numberField.getText(); // grabbed test from number field
+        firstNumber = Long.parseLong(numberAsText); // stored it as an int instead of string
+        numberField.setText("");
+        operation = OperatorType.Divide;
+    }
+
+
 }
